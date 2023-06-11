1360ffaed290abf368b00e2c7d77408d04f25a2a
Ruan Oliveira <ruanamaral40@gmail.com>
2023-06-10 23:53:39 -0300

    feat: adicionando autenticação firebase no registro criando validações e cenário de erro

---
+++ src/Views/Register/constants.ts
@@ -0,0 +1,14 @@
+export const errorType = {
+    incorrectCredentials:
+        "Ops, credenciais inválidas. Corrija e Tente novamente!",
+    existingEmail: "Ops, Esse email já foi cadastrado. Tente outro email!",
+};
+
+export const emailAlreadyInUse = "auth/email-already-in-use"
+
+export const initialStates = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
--- src/Views/Register/index.tsx
+++ src/Views/Register/index.tsx
@@ -1,74 +1,151 @@
 import React from "react";
 
 import {
-  AuthenticateButton,
-  AuthenticateTemplate,
-  InputText,
+    AuthenticateButton,
+    AuthenticateTemplate,
+    InputText,
+    Loading,
 } from "../../Components";
 
 import * as S from "./styles";
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase/firebaseConfig";
+import { emailAlreadyInUse, errorType, initialStates } from "./constants";
+import { SucessfulRegistration } from "./SuccessfulRegistration";
 
 const Register = () => {
-  return (
-    <AuthenticateTemplate>
-      <S.StyledRegisterWrapper>
-        <S.StyledTitle>Olá,</S.StyledTitle>
-        <S.StyledDescription>
-          Para continuar navegando de forma segura, efetue o cadastro na rede.
-        </S.StyledDescription>
-        <S.StyledFormWrapper>
-          <S.StyledFormTitle>Cadastro</S.StyledFormTitle>
-          <InputText
-            type="text"
-            label="Nome"
-            placeholder="Digite seu nome"
-            onChange={function (
-              event: React.ChangeEvent<HTMLInputElement>
-            ): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <InputText
-            type="text"
-            label="Email"
-            placeholder="Digite seu email"
-            onChange={function (
-              event: React.ChangeEvent<HTMLInputElement>
-            ): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <InputText
-            type="password"
-            label="Senha"
-            placeholder="Digite sua senha"
-            onChange={function (
-              event: React.ChangeEvent<HTMLInputElement>
-            ): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <InputText
-            type="password"
-            label="Confirmar senha"
-            placeholder="Confirme sua senha"
-            onChange={function (
-              event: React.ChangeEvent<HTMLInputElement>
-            ): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <S.StyledButtonWrapper>
-            <AuthenticateButton text="Cadastrar" />
-            <S.StyledLink>
-              Caso já possua uma conta <Link to="/">Clique Aqui</Link>
-            </S.StyledLink>
-          </S.StyledButtonWrapper>
-        </S.StyledFormWrapper>
-      </S.StyledRegisterWrapper>
-    </AuthenticateTemplate>
-  );
+    const [error, setError] = React.useState<string>("");
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [successfulRegistration, setSuccessfulRegistration] =
+        React.useState<boolean>(false);
+
+    const [fieldControl, setFieldControl] = React.useState(initialStates);
+
+    const [errorStyle, setErrorStyle] = React.useState({
+        fullName: false,
+        email: false,
+        password: false,
+        confirmPassword: false,
+    });
+
+    const { fullName, email, password, confirmPassword } = fieldControl;
+
+    const validate = () => {
+        const regEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+        const regName = /^([A-Za-z\u00C0-\u017F´]\s?){3,40}$/g;
+
+        const ErrorObj = {
+            ...errorStyle,
+            fullName: !regName.test(fullName),
+            email: !regEmail.test(email),
+            password: password.length < 5,
+            confirmPassword: confirmPassword !== password || !password,
+        };
+
+        setErrorStyle(ErrorObj);
+
+        if (
+            Object.values(ErrorObj).find(
+                (attribute: boolean) => attribute === true
+            )
+        )
+            return setError(errorType.incorrectCredentials);
+
+        createUser();
+    };
+
+    const createUser = async () => {
+        setLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => setSuccessfulRegistration(true))
+            .catch((error) =>
+                error.code === emailAlreadyInUse
+                    ? setError(errorType.existingEmail)
+                    : setError(errorType.incorrectCredentials)
+            )
+            .finally(() => setLoading(false));
+    };
+
+    return (
+        <AuthenticateTemplate>
+            <S.StyledRegisterWrapper>
+                <S.StyledTitle>Olá,</S.StyledTitle>
+                <S.StyledDescription>
+                    Para continuar navegando de forma segura, efetue o cadastro
+                    na rede.
+                </S.StyledDescription>
+                <S.StyledFormWrapper>
+                    <S.StyledFormTitle>Cadastro</S.StyledFormTitle>
+                    <InputText
+                        type="text"
+                        label="Nome"
+                        placeholder="Digite seu nome"
+                        error={errorStyle.fullName}
+                        value={fieldControl.fullName}
+                        onChange={({ target }) =>
+                            setFieldControl({
+                                ...fieldControl,
+                                fullName: target.value,
+                            })
+                        }
+                    />
+                    <InputText
+                        type="text"
+                        label="Email"
+                        placeholder="Digite seu email"
+                        error={errorStyle.email}
+                        value={fieldControl.email}
+                        onChange={({ target }) =>
+                            setFieldControl({
+                                ...fieldControl,
+                                email: target.value,
+                            })
+                        }
+                    />
+                    <InputText
+                        type="password"
+                        label="Senha"
+                        placeholder="Digite sua senha"
+                        error={errorStyle.password}
+                        value={fieldControl.password}
+                        onChange={({ target }) =>
+                            setFieldControl({
+                                ...fieldControl,
+                                password: target.value,
+                            })
+                        }
+                    />
+                    <InputText
+                        type="password"
+                        label="Confirmar senha"
+                        placeholder="Confirme sua senha"
+                        value={fieldControl.confirmPassword}
+                        error={errorStyle.confirmPassword}
+                        onChange={({ target }) =>
+                            setFieldControl({
+                                ...fieldControl,
+                                confirmPassword: target.value,
+                            })
+                        }
+                    />
+                    <S.StyledButtonWrapper>
+                        {!!error && <span>{error}</span>}
+                        <AuthenticateButton
+                            text="Cadastrar"
+                            onClick={validate}
+                        />
+                        <S.StyledLink>
+                            Caso já possua uma conta{" "}
+                            <Link to="/">Clique Aqui</Link>
+                        </S.StyledLink>
+                    </S.StyledButtonWrapper>
+                </S.StyledFormWrapper>
+            </S.StyledRegisterWrapper>
+            {successfulRegistration && <SucessfulRegistration />}
+            <Loading visible={loading} />
+        </AuthenticateTemplate>
+    );
 };
 
 export default Register;
