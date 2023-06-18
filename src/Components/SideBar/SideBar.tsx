c29c4a24597d0872c4eaf3ea1f5c3ff05943b005
Ruan Oliveira <ruanamaral40@gmail.com>
2023-06-18 19:48:45 -0300

    feat: adicionando botão para disconect no menu

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -5,21 +5,37 @@ import Logo from "../../assets/logo.png";
 import { MenuList } from "./constants";
 import { MenuSideBar } from "./MenuSibeBar";
 
+import SingOutImg from "../../assets/sairIcon.png";
+import { auth } from "../../Firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 export const SideBar: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleDisconect = () => {
+        auth.signOut().then(() => navigate("/"));
+    };
+
     return (
         <S.SideBarContainer>
-            <S.StyledLogo src={Logo} />
-            <S.SideBarItensContainer>
-                {MenuList.map((menuItem, index) => (
-                    <MenuSideBar
-                        key={index.toString()}
-                        route={menuItem.route}
-                        name={menuItem.name}
-                        iconOnFocus={menuItem.iconOnFocus}
-                        iconOutFocus={menuItem.iconOutFocus}
-                    />
-                ))}
-            </S.SideBarItensContainer>
+            <S.SideBarMenuWrapper>
+                <S.StyledLogo src={Logo} />
+                <S.SideBarItensContainer>
+                    {MenuList.map((menuItem, index) => (
+                        <MenuSideBar
+                            key={index.toString()}
+                            route={menuItem.route}
+                            name={menuItem.name}
+                            iconOnFocus={menuItem.iconOnFocus}
+                            iconOutFocus={menuItem.iconOutFocus}
+                        />
+                    ))}
+                </S.SideBarItensContainer>
+            </S.SideBarMenuWrapper>
+            <S.SingOutWrapper onClick={handleDisconect}>
+                <S.SingOutImg src={SingOutImg} />
+                <S.ProfileSingOut>Sair</S.ProfileSingOut>
+            </S.SingOutWrapper>
         </S.SideBarContainer>
     );
 };
--- src/Components/SideBar/styles.ts
+++ src/Components/SideBar/styles.ts
@@ -10,6 +10,7 @@ max-width: 175px;
 background: #7C3AED;
 min-height: 100vh;
 box-sizing: border-box;
+justify-content: space-between;
 `
 
 export const StyledLogo = styled.img`
@@ -22,3 +23,28 @@ flex-direction: column;
 gap: 36px;
 `
 
+export const SideBarMenuWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const ProfileSingOut = styled.span`
+color:  white;
+transition: all 300ms ease-in-out 0s;
+cursor: pointer;
+
+`
+
+export const SingOutWrapper = styled.div`
+display: flex;
+gap: 10px;
+align-items: center;
+cursor: pointer;
+`
+
+export const SingOutImg = styled.img`
+width: 15px;
+height: 15px;
+color: white;
+`
