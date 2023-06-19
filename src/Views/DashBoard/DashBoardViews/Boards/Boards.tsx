d6dabff672b95483c299474da6439c1706556c46
Ruan Oliveira <ruanamaral40@gmail.com>
2023-06-18 22:18:03 -0300

    feat: crinado botão para criar task

--- src/Views/DashBoard/DashBoardViews/Boards/Boards.tsx
+++ src/Views/DashBoard/DashBoardViews/Boards/Boards.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import * as S from "./styles";
 import { BoardHeader } from "./BoardHeader";
+import { BoardContent } from "./BoardContent";
+import { AuthenticateButton } from "../../../../Components";
+import useColumnTasks from "../../../../Hooks/useColumnTasks";
+import { ColumnType } from "../../../../Utils/enums";
 
 export const Boards: React.FC = () => {
+    const { addEmptyTask } = useColumnTasks(ColumnType.TO_DO);
     return (
         <S.BoardWrapper>
             <BoardHeader />
+            <S.NewTaskWrapperButton>
+                <AuthenticateButton
+                    text="Criar nova tarefa"
+                    onClick={addEmptyTask}
+                />
+            </S.NewTaskWrapperButton>
+            <BoardContent />
         </S.BoardWrapper>
     );
 };
