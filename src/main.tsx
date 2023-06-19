7df0616a4af8a6ca43693d0950ab1c6c1c7e4607
Ruan Oliveira <ruanamaral40@gmail.com>
2023-06-18 22:16:02 -0300

    feat: adicionando novos providers

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./Routes/router";
 import AuthProvider from "./Context/AuthContext";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <AuthProvider>
-            <Router />
-        </AuthProvider>
+        <ChakraProvider>
+            <DndProvider backend={HTML5Backend}>
+                <AuthProvider>
+                    <Router />
+                </AuthProvider>
+            </DndProvider>
+        </ChakraProvider>
     </React.StrictMode>
 );
